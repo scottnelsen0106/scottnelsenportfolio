1618bbaa365c67871182a52b9ef43de604fe6baa
Scott Nelsen <snelsen0106@gmail.com>
2020-01-26 14:17:49 -0600

    took build folder out of .gitignore

---
+++ client/build/precache-manifest.090c9d6e6e42737e1ea6c6d6ecd8e90b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2051b643ce6155962517eda9b517714a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f72309b732e09ced0b0",
+    "url": "/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "ece1e7a15456ecb7ced5",
+    "url": "/static/css/main.95175642.chunk.css"
+  },
+  {
+    "revision": "8f72309b732e09ced0b0",
+    "url": "/static/js/2.d95ee081.chunk.js"
+  },
+  {
+    "revision": "c7abd4a823ee85afac2f05677368b7de",
+    "url": "/static/js/2.d95ee081.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ece1e7a15456ecb7ced5",
+    "url": "/static/js/main.a7a01360.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
